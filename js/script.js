a268833cfeb85e284c25ed68fbcd181e473c3300
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2026-06-16 08:00:38 -0300

    update

--- js/script.js
+++ js/script.js
@@ -6,13 +6,28 @@
 // soma(3, 4, 1)
 
 
-function soma(...termos) {
-    let result = 0;
 
-    for (let i = 0; i < termos.length; i++) {
-        result += termos[i];
+
+
+// function soma(...termos) {
+//     let result = 0;
+
+//     for (let i = 0; i < termos.length; i++) {
+//         result += termos[i];
         
-    }
+//     }
 
-    return result;
-}
+//     return result;
+// }
+
+// console.log(soma(5, 2, 7 ,3));
+
+
+
+
+// function mostrarHora() {
+//     let data = new Date();
+//     console.log(data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds());
+// }
+
+// console.log(mostrarHora());
